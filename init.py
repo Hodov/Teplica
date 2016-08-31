bfbf1688c0e2b97946c12a3a9aa632eb9747a35b
hodov <ihodov@gmail.com>
2016-08-31 17:36:21 +0300

    Changes in settings file

--- init.py
+++ init.py
@@ -7,7 +7,7 @@ pipes = [[0xE8, 0XE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
 
 
 def initAll():
-    radio.initRadio(radio.getPipeFromString(settings.getPipe()))
+    radio.initRadio(radio.getPipeFromString(settings.get_pipe()))
     sensors.initStorage()
     grafana.init()
     sensors.initAllRelays()
