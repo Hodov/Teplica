14a56960a46ff398da503b2caf5b2d030214ee69
hodov <ihodov@gmail.com>
2016-09-06 16:01:12 +0300

    logging

--- log.py
+++ log.py
@@ -3,12 +3,12 @@ import settings
 
 logger = logging.getLogger('main')
 
+
 def init():
     log_level = settings.get_log_level()
     formatter = logging.Formatter(u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] %(message)s')
     ch = logging.StreamHandler()
-    fh = logging.FileHandler(u'logfile.log')
-
+    fh = logging.FileHandler(u'Logs/logfile.log')
     if log_level == 'debug':
         level = logging.DEBUG
     elif log_level == 'info':
@@ -19,14 +19,10 @@ def init():
         level = logging.ERROR
     else:
         level = logging.CRITICAL
-
     logger.setLevel(level)
-
     fh.setLevel(level)
     ch.setLevel(logging.ERROR)
-
     ch.setFormatter(formatter)
     fh.setFormatter(formatter)
     logger.addHandler(ch)
     logger.addHandler(fh)
-
